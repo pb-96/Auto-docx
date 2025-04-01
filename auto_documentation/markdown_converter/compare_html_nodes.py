e675197e199c0729aaad8034dab7de80ea179e5a
pb-96 <philip.byrne@faculty.ai>
2025-04-01 15:18:40 +0100

    adding tester function for testing

---
+++ auto_documentation/markdown_converter/compare_html_nodes.py
@@ -0,0 +1,21 @@
+from html_validator import HtmlNode
+
+
+def compare_nodes_equal(left: HtmlNode, right: HtmlNode) -> bool:
+    if left.tag != right.tag:
+        return False
+
+    if left.children and not right.children:
+        return False
+
+    if right.children and not left.children:
+        return False
+
+    if len(right.children) != len(left.children):
+        return False
+
+    for left_child, right_child in zip(left.children, right.children):
+        if not compare_nodes_equal(left_child, right_child):
+            return False
+    
+    return True
