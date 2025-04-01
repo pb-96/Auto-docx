943c40483f6e76e3a5e81316be9b24476ce45c76
pb-96 <philip.byrne@faculty.ai>
2025-04-01 16:07:32 +0100

    adding updated linting

--- tests/html_validator_test.py
+++ tests/html_validator_test.py
@@ -1,12 +1,7 @@
 import unittest
 from pathlib import Path
-from auto_documentation.markdown_converter.html_validator import (
-    HTMLProcessor,
-    HtmlNode
-)
-from auto_documentation.markdown_converter.compare_html_nodes import (
-    compare_nodes_equal
-)
+from auto_documentation.markdown_converter.html_validator import HTMLProcessor, HtmlNode
+from auto_documentation.markdown_converter.compare_html_nodes import compare_nodes_equal
 from auto_documentation.markdown_converter.markdown import (
     parse,
 )
@@ -18,13 +13,12 @@ class HtmlValidatorTest(unittest.TestCase):
         AS_HTML = HTMLProcessor(COMPLEX_DATA).root
         is_equal = compare_nodes_equal(AS_HTML, AS_HTML)
         assert is_equal
-    
+
     def test_not_equal(self):
         COMPLEX_DATA = Path("tests/test_data/test.html").read_text()
         AS_HTML = HTMLProcessor(COMPLEX_DATA).root
         SIMPLE_DATA = Path("tests/test_data/simple.html").read_text()
         AS_OTHER = HTMLProcessor(SIMPLE_DATA).root
-
         not_equal = compare_nodes_equal(AS_HTML, AS_OTHER)
         assert not not_equal
 
@@ -34,7 +28,3 @@ class HtmlValidatorTest(unittest.TestCase):
         as_html = parse(MARKDOWN_DATA)
         as_html_node = HTMLProcessor(as_html).root
         AS_HTML = HTMLProcessor(COMPLEX_DATA).root
-
-        
-        
-
