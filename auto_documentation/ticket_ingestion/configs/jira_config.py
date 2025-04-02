23075ff77d25f1ebe5775995dbeff0a0c5735367
pb-96 <philip.byrne@faculty.ai>
2025-04-02 14:11:38 +0100

    adding ticket base class

--- auto_documentation/ticket_ingestion/configs/jira_config.py
+++ auto_documentation/ticket_ingestion/configs/jira_config.py
@@ -1,57 +1,11 @@
 from pydantic import BaseModel
-from typing import List, Union, Literal
-from enum import Enum
-
-
-class ActionType(str, Enum):
-    DESCRIPTION = "Description"
-    TEST = "Test"
+from auto_documentation.ticket_ingestion.configs.ticket_tree import TicketTree
 
 
 class JiraConfig(BaseModel):
     # Would need to store the passwords correctly
     email: str
     auth: str
-
-
-class TicketTree(BaseModel):
-    # Basically this the model that maps from the parent ticket to the test
-    parent: Union["TicketTree", None]
-    ticket_name: str
-    ticket_type: str
-    child: List["TicketTree"] = []
-    action: ActionType = ActionType.DESCRIPTION
-
-    def __init__(self, **data):
-        super().__init__(**data)
-
-    def relationship_pointer(self, string: str, indent: int = 0):
-        left = "-" * indent
-        return f"{left}>{string}\n"
-
-    def display_relationship(self):
-        initial_index = 0
-        result_str = self.relationship_pointer(self.ticket_type, initial_index)
-        # BFS style display
-        for _child in self.child:
-            initial_index = 2
-            result_str += self.relationship_pointer(
-                _child.ticket_type, indent=initial_index
-            )
-            next_children = _child.child
-            if len(next_children):
-                initial_index = initial_index + 2
-                while next_children:
-                    next_child = next_children.pop()
-                    result_str += self.relationship_pointer(
-                        next_child.ticket_type, indent=initial_index + 2
-                    )
-                    next_children.extend(next_child.child)
-                    initial_index += 2
-        return result_str
-
-    def __repr__(self):
-        return self.display_relationship()
 
 
 JIRA_INSTANCE = TicketTree(
