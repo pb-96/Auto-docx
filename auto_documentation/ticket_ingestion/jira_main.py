a9b374e62dd3e783c14ab628503623d366d58053
pb-96 <philip.byrne@faculty.ai>
2025-04-02 14:18:29 +0100

    fixingSpellingMistake

--- auto_documentation/ticket_ingestion/jira_main.py
+++ auto_documentation/ticket_ingestion/jira_main.py
@@ -1,14 +1,20 @@
 from typing import Dict
 from jira import JIRA
 from auto_documentation.ticket_ingestion.configs.jira_config import JiraConfig
+from auto_documentation.ticket_ingestion.configs.ticket_tree import TicketTree
 
 
-class IngestJita:
-    def __init__(self, jira_config: JiraConfig):
+class IngestJira:
+    def __init__(
+        self, jira_config: JiraConfig, ticket_tree: TicketTree, parent_ticket_id: str
+    ):
         self.jira_config = jira_config
         self.jira_object = JIRA()
+        self.ticket_tree = ticket_tree
 
     def perform_auth(self):
         email, auth = self.jira_config.email, self.jira_config.auth
 
-    def get_jira_issues(self): ...
+    def build_ticket_queries(self): ...
+
+    def get_issues(self): ...
