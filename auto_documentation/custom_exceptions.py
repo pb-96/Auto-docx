8b59589a7c418d98704a0e78fe2e41d8f37cd2f8
pb-96 <philip.byrne@faculty.ai>
2025-04-15 18:18:28 +0100

    fixing pass issues

--- auto_documentation/custom_exceptions.py
+++ auto_documentation/custom_exceptions.py
@@ -2,7 +2,5 @@ class PromptBuilderError(Exception):
     """Base exception for PromptBuilder errors."""
 
 
-
 class InvalidTicketStructureError(PromptBuilderError):
     """Raised when the ticket structure is invalid."""
-
