5bbf8d3fae3919309d8e92073ded0f3351521550
pb-96 <philip.byrne@faculty.ai>
2025-04-29 17:08:37 +0100

    updating sh command

--- example_run.sh
+++ example_run.sh
@@ -1,3 +1,3 @@
 #/bin/bash
 
-python3 auto_documentation/main.py  --run-type=DOX_GENERATE --ticket-source=JIRA --ticket-tree-src=tests/test_data/example_config.yaml --parent-ticket-id=MBA-15 --output-file-path=example.docx --document-type=word --test-folder=any
+python3 auto_documentation/main.py  --run-type=DOX_GENERATE --ticket-source=JIRA --ticket-tree-src=tests/test_data/example_config.yaml --parent-ticket-id=MBA-15 --output-file-path=example.docx --document-type=word --test-folder=tests/test_data/example_test_folder/test_MBA-14.py
