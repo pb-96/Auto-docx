166d407aac69e2fdde5bb9dea68ef1bd1a0fe96e
pb-96 <philip.byrne@faculty.ai>
2025-08-20 15:53:39 +0100

    removing contents of prompts.py

--- auto_documentation/prompt_builder/prompt_parser.py
+++
@@ -1,8 +0,0 @@
-from auto_documentation.custom_types import PromptOutput
-from pathlib import Path
-from typing import Tuple, Union
-import json
-
-
-def parse_prompt_output(prompt_output_path: Union[Path, str]) -> Tuple[Path, Path]:
-    ...
